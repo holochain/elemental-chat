1ceab2de326855bd583016da8b3b29a522110f68
Eric Harris-Braun <eric@harris-braun.com>
2020-11-19 16:23:33 -0500

    remove failing debugs

--- zomes/chat/src/entries/channel/handlers.rs
+++ zomes/chat/src/entries/channel/handlers.rs
@@ -47,7 +47,6 @@ pub(crate) fn create_channel(channel_input: ChannelInput) -> ChatResult<ChannelD
 }
 
 pub(crate) fn list_channels(list_channels_input: ChannelListInput) -> ChatResult<ChannelList> {
-    debug!("list_channels")?;
     // Get the category path
     let path = Path::from(list_channels_input.category);
 
@@ -97,8 +96,6 @@ pub(crate) fn list_channels(list_channels_input: ChannelListInput) -> ChatResult
             }
         }
     }
-    debug!("list_channels")?;
-    debug!("{:?}", channels)?;
     // Return all the channels data to the UI
     Ok(channels.into())
 }
