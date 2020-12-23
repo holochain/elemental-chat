e72e846fb96fd4315ad4282574c71db27d877ff7
Eric Harris-Braun <eric@harris-braun.com>
2020-12-23 09:47:56 -0500

    re-enable tests

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -2,8 +2,8 @@ import { Orchestrator } from '@holochain/tryorama'
 
 const orchestrator = new Orchestrator()
 
-// require('./elemental-chat')(orchestrator)
-// require('./chat-signals')(orchestrator)
+require('./elemental-chat')(orchestrator)
+require('./chat-signals')(orchestrator)
 require('./chat-stats')(orchestrator)
 
 orchestrator.run()
