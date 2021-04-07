c0a06d1476cb295ffaac5ecccce2d7395412961e
Eric Harris-Braun <eric@harris-braun.com>
2021-04-07 13:18:43 -0400

    Update release script to build non-uid version as well

--- release-builds.sh
+++ release-builds.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 build () {
-    sed -i "s/uid: .*/uid: \"$2\"/" happ.yaml
-    hc app pack . -o elemental-chat.$1.$2.happ
+    if [[ $2 = "~" ]]
+    then
+        sed -i "s/uid: .*/uid: \~/" happ.yaml
+        FILE="elemental-chat.$1.happ"
+    else
+        sed -i "s/uid: .*/uid: \"$2\"/" happ.yaml
+        FILE="elemental-chat.$1.$2.happ"
+    fi
+    hc app pack . -o $FILE
 }
 # get the version from the chat zome Cargo.toml
 VERSION=`grep -Po '^version = "\K([^"]+)' zomes/chat/Cargo.toml | sed -e "s/[.-]/_/g"`
 build $VERSION 0002
 build $VERSION 0001
 build $VERSION develop
+build $VERSION "~"
