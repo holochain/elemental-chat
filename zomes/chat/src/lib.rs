23c9d268783f2f3c508156acf130c3b6d260f85b
zo-el <joelulahanna@gmail.com>
2021-04-28 16:52:25 +0530

    clean pkgs

--- zomes/chat/src/lib.rs
+++ zomes/chat/src/lib.rs
@@ -53,7 +53,7 @@ entry_defs![
 #[hdk_extern]
 fn init(_: ()) -> ExternResult<InitCallbackResult> {
     // grant unrestricted access to accept_cap_claim so other agents can send us claims
-    let mut functions = HashSet::new();
+    let mut functions = BTreeSet::new();
     functions.insert((zome_info()?.zome_name, "recv_remote_signal".into()));
     create_cap_grant(CapGrantEntry {
         tag: "".into(),
