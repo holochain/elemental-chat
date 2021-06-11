bdaf50e8e3fd77d36c63a4894bee61b2487e1289
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-06-11 11:38:50 +0200

    Merged remove joining agent validation

---
+++ crates/joining_code/src/lib.rs
@@ -0,0 +1,9 @@
+mod joining_code;
+mod validation;
+mod props;
+mod init;
+
+pub use joining_code::*;
+pub use validation::*;
+pub use init::*;
+pub use props::*;
---
+++ crates/joining_code/src/props.rs
@@ -0,0 +1,37 @@
+use hdk::prelude::*;
+use holo_hash::AgentPubKeyB64;
+
+#[derive(Debug, Serialize, Deserialize, SerializedBytes, Clone)]
+pub struct Props {
+    pub skip_proof: bool,
+    pub holo_agent_override: Option<AgentPubKeyB64>,
+}
+
+// zome_info()?.properties
+pub fn holo_agent(encoded_props: &SerializedBytes) -> ExternResult<AgentPubKey> {
+    let maybe_props = Props::try_from(encoded_props.to_owned());
+    if let Ok(props) = maybe_props {
+        if let Some(a) = props.holo_agent_override {
+            return Ok(AgentPubKey::try_from(a).unwrap())
+        }
+    }
+    // This is a hard coded holo agent public key
+    return Ok(AgentPubKey::try_from("uhCAkfzycXcycd-OS6HQHvhTgeDVjlkFdE2-XHz-f_AC_5xelQX1N").unwrap())
+}
+
+
+pub fn skip_proof_sb(encoded_props: &SerializedBytes) -> bool {
+    let maybe_props = Props::try_from(encoded_props.to_owned());
+    if let Ok(props) = maybe_props {
+        return props.skip_proof;
+    }
+    false
+}
+
+// This is useful for test cases where we don't want to provide a membrane proof
+pub fn skip_proof() -> bool {
+    if let Ok(info) = zome_info() {
+        return skip_proof_sb(&info.properties);
+    }
+    return false;
+}
