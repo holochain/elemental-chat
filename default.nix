df79f08cdf9a66e3ef14ef6da0c697b8b28549db
zo-el <joelulahanna@gmail.com>
2021-08-24 16:14:00 +0530

    update lair

--- default.nix
+++ default.nix
@@ -1,22 +1,23 @@
 let
-  holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/cedfc2453cfa795a0344acd6f5fb302362e18fc5.tar.gz";
-    sha256 = "sha256:004s7lkfhb5lg5292b80byx8b8zdm8lc3g0ldhfx9biqdg6m9agp";
-  };
+  holonixPath = builtins.fetchTarball https://github.com/holochain/holonix/archive/6ae8ffb8e5c1a1faa4f4e1af8a9f7139b2ce0f3c.tar.gz;
   holonix = import (holonixPath) {
     includeHolochainBinaries = true;
     holochainVersionId = "custom";
 
     holochainVersion = {
-     rev = "0d1c06630000beb9e3c5f6e54ae85cbe9ffa484a";
-     sha256 = "1shjal90zxrqyz9abzw2a3m4p54g26pj752bs0cxicc71vwgzhy0";
-     cargoSha256 = "sha256:0nvqimhfal82imvhrc5ahp1khp7779n8i3zkcgf2yps9b0myxr11";
-     bins = {
-       holochain = "holochain";
-       hc = "hc";
-     };
+      rev = "0d1c06630000beb9e3c5f6e54ae85cbe9ffa484a";
+      sha256 = "1shjal90zxrqyz9abzw2a3m4p54g26pj752bs0cxicc71vwgzhy0";
+      cargoSha256 = "sha256:0nvqimhfal82imvhrc5ahp1khp7779n8i3zkcgf2yps9b0myxr11";
+      bins = {
+        holochain = "holochain";
+        hc = "hc";
+      };
+
+      lairKeystoreHashes = {
+        sha256 = "1jiz9y1d4ybh33h1ly24s7knsqyqjagsn1gzqbj1ngl22y5v3aqh";
+        cargoSha256 = "0agykcl7ysikssfwkjgb3hfw6xl0slzy38prc4rnzvagm5wd1jjv";
+      };
     };
-    holochainOtherDepsNames = ["lair-keystore"];
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
