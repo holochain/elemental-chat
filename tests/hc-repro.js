23a7dfe79192d0a60ebb7e3031e442b003b17ab7
timotree3 <timorcb@gmail.com>
2021-09-29 19:35:44 -0400

    Fix issue by changing tuning_params

--- tests/hc-repro.js
+++ tests/hc-repro.js
@@ -23,31 +23,27 @@ const network = {
       }
     }
   ],
+
   tuning_params: {
     gossip_strategy: "sharded-gossip",
-    gossip_loop_iteration_delay_ms: "1000",
-    gossip_outbound_target_mbps: "0.5",
-    gossip_inbound_target_mbps: "0.5",
-    gossip_historic_outbound_target_mbps: "0.1",
-    gossip_historic_inbound_target_mbps: "0.1",
-    gossip_peer_on_success_next_gossip_delay_ms: "60000",
-    gossip_peer_on_error_next_gossip_delay_ms: "300000",
-    gossip_local_sync_delay_ms: "60000",
-    gossip_dynamic_arcs: "false",
-    gossip_single_storage_arc_per_space: "false",
-    default_rpc_single_timeout_ms: "30000",
-    default_rpc_multi_remote_agent_count: "3",
-    default_rpc_multi_remote_request_grace_ms: "3000",
-    agent_info_expires_after_ms: "1200000",
-    tls_in_mem_session_storage: "512",
-    proxy_keepalive_ms: "120000",
-    proxy_to_expire_ms: "300000",
-    concurrent_limit_per_thread: "4096",
-    tx2_quic_max_idle_timeout_ms: "30000",
-    tx2_pool_max_connection_count: "4096",
-    tx2_channel_count_per_connection: "16",
-    tx2_implicit_timeout_ms: "30000",
-    tx2_initial_connect_retry_delay_ms: "200"
+    gossip_loop_iteration_delay_ms: 2000,
+    default_notify_remote_agent_count:  5,
+    default_notify_timeout_ms: 1000 * 30,
+    default_rpc_single_timeout_ms: 1000 * 30,
+    default_rpc_multi_remote_agent_count: 1,
+    default_rpc_multi_timeout_ms: 1000 * 30,
+    agent_info_expires_after_ms: 1000 * 60 * 20,
+    tls_in_mem_session_storage: 512,
+    proxy_keepalive_ms: 1000 * 60 * 2,
+    proxy_to_expire_ms: 1000 * 60 * 5,
+    concurrent_limit_per_thread: 4096,
+    tx2_quic_max_idle_timeout_ms: 1000 * 30,
+    tx2_pool_max_connection_count: 4096,
+    tx2_channel_count_per_connection: 16,
+    tx2_implicit_timeout_ms: 1000 * 30,
+    tx2_initial_connect_retry_delay_ms: 200,
+    default_rpc_multi_remote_request_grace_ms: 10,
+    gossip_single_storage_arc_per_space: true
   }
 }
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
