7ec8816452646968cfb41842323e97c608034d74
zo-el <joelulahanna@gmail.com>
2021-10-11 00:55:14 +0530

    update read-only validation

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,35 +1,35 @@
 import { Orchestrator } from '@holochain/tryorama'
 
 let orchestrator = new Orchestrator()
-require('./basic-chatting')(orchestrator)
-orchestrator.run()
+// require('./basic-chatting')(orchestrator)
+// orchestrator.run()
 
-orchestrator = new Orchestrator()
-require('./transient-nodes')(orchestrator)
-orchestrator.run()
+// orchestrator = new Orchestrator()
+// require('./transient-nodes')(orchestrator)
+// orchestrator.run()
 
-/* currently not using multi-chunk
-orchestrator = new Orchestrator()
-require('./multi-chunk')(orchestrator)
-orchestrator.run()
-*/
+// /* currently not using multi-chunk
+// orchestrator = new Orchestrator()
+// require('./multi-chunk')(orchestrator)
+// orchestrator.run()
+// */
 
-orchestrator = new Orchestrator()
-require('./chat-signals')(orchestrator)
-orchestrator.run()
+// orchestrator = new Orchestrator()
+// require('./chat-signals')(orchestrator)
+// orchestrator.run()
 
-orchestrator = new Orchestrator()
-require('./chat-stats')(orchestrator)
-orchestrator.run()
+// orchestrator = new Orchestrator()
+// require('./chat-stats')(orchestrator)
+// orchestrator.run()
 
-orchestrator = new Orchestrator()
-require('./profile')(orchestrator)
-orchestrator.run()
+// orchestrator = new Orchestrator()
+// require('./profile')(orchestrator)
+// orchestrator.run()
 
 orchestrator = new Orchestrator()
 require('./membrane-proof')(orchestrator)
 orchestrator.run()
 
-orchestrator = new Orchestrator()
-require('./unique-registration-code')(orchestrator)
-orchestrator.run()
+// orchestrator = new Orchestrator()
+// require('./unique-registration-code')(orchestrator)
+// orchestrator.run()
