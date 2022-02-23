29e1dbc31be3910345d915a9ab4578d825d3e09b
zo-el <joelulahanna@gmail.com>
2022-02-22 23:00:49 -0600

    comment in all tests

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,33 +1,33 @@
 import { Orchestrator } from '@holochain/tryorama'
 
 let orchestrator = new Orchestrator()
-// require('./basic-chatting')(orchestrator)
-// orchestrator.run()
+require('./basic-chatting')(orchestrator)
+orchestrator.run()
 
 orchestrator = new Orchestrator()
 require('./transient-nodes')(orchestrator)
 orchestrator.run()
 
-// orchestrator = new Orchestrator()
-// require('./chat-signals')(orchestrator)
-// orchestrator.run()
+orchestrator = new Orchestrator()
+require('./chat-signals')(orchestrator)
+orchestrator.run()
 
-// orchestrator = new Orchestrator()
-// require('./chat-stats')(orchestrator)
-// orchestrator.run()
+orchestrator = new Orchestrator()
+require('./chat-stats')(orchestrator)
+orchestrator.run()
 
-// orchestrator = new Orchestrator()
-// require('./profile')(orchestrator)
-// orchestrator.run()
+orchestrator = new Orchestrator()
+require('./profile')(orchestrator)
+orchestrator.run()
 
-// orchestrator = new Orchestrator()
-// require('./membrane-proof')(orchestrator)
-// orchestrator.run()
+orchestrator = new Orchestrator()
+require('./membrane-proof')(orchestrator)
+orchestrator.run()
 
-// orchestrator = new Orchestrator()
-// require('./unique-registration-code')(orchestrator)
-// orchestrator.run()
+orchestrator = new Orchestrator()
+require('./unique-registration-code')(orchestrator)
+orchestrator.run()
 
-// orchestrator = new Orchestrator()
-// require('./batching')(orchestrator)
-// orchestrator.run()
+orchestrator = new Orchestrator()
+require('./batching')(orchestrator)
+orchestrator.run()
