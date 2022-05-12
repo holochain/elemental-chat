6e76642e7f0aaf95bd49adeba1bb82930df5a051
Michael Dougherty <maackle.d@gmail.com>
2022-05-12 10:10:39 -0700

    Fix nix holochain version

--- config.nix
+++ config.nix
@@ -2,5 +2,5 @@
   # git ls-remote https://github.com/holochain/holonix staging
   holonixRevision = "44542b5c0cdc984454fad96fbec313b7b1c8ca7c";
 
-  holochainVersionId = "v0_0_132";
+  holochainVersionId = "v0_0_131";
 }
