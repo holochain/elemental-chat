7eef818223d0dbc4607b439104c206f125695e63
peeech <pj@imagine-nyc.com>
2022-07-21 09:41:34 +0200

    bump nix env to holochain v0.0.145

--- default.nix
+++ default.nix
@@ -1,27 +1,14 @@
 let
-  holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/cedfc2453cfa795a0344acd6f5fb302362e18fc5.tar.gz";
-    sha256 = "sha256:004s7lkfhb5lg5292b80byx8b8zdm8lc3g0ldhfx9biqdg6m9agp";
-  };
+  holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
-    includeHolochainBinaries = true;
     holochainVersionId = "custom";
-
-    holochainVersion = {
-     rev = "bd89d55e397baf7876099f600db997c89dd70fb6";
-     sha256 = "1nbq8qq4hzww5460khhv5ihj76bsnfqs306dcyknb2rq2firl1m1";
-     cargoSha256 = "sha256:1hbdcks7hkd3924wjy9qkizyn2hvhdvm14m23sjl9nn3xygj8w7w";
-     bins = {
-       holochain = "holochain";
-       hc = "hc";
-     };
-    };
-    holochainOtherDepsNames = ["lair-keystore"];
+    holochainVersion = import ./holochain_version.nix;
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
-  buildInputs = with nixpkgs; [
-    binaryen
+  packages = with nixpkgs; [
+    niv
+    nixpkgs.binaryen
   ];
 }
---
+++ holochain_version.nix
@@ -0,0 +1,37 @@
+# This file was generated with the following command:
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/develop_lair_0_1.nix --bins-filter=holochain,hc,kitsune-p2p-proxy,kitsune-p2p-tx2-proxy --git-src=branch:develop --lair-version-req=~0.1
+# For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
+
+{
+    url = "https://github.com/holochain/holochain";
+    rev = "holochain-0.0.145";
+    sha256 = "18pkrnf9p0b6hriyf9jk52hds6d10h6z8bkaxmigl1xv4vwm2a1p";
+    cargoLock = {
+        outputHashes = {
+        };
+    };
+
+    binsFilter = [
+        "holochain"
+        "hc"
+        "kitsune-p2p-proxy"
+        "kitsune-p2p-tx2-proxy"
+    ];
+
+
+    lair = {
+        url = "https://github.com/holochain/lair";
+        rev = "v0.0.10";
+        sha256 = "sha256-yBCsdtC6vYnGQL1JkLkOzECk1TD4RoFzNARdsc+J0cg=";
+
+        binsFilter = [
+            "lair-keystore"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+} 
